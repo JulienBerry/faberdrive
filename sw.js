63507495972c136961fdd432868734305a2bbaa5
Julien <126426809+JulienBerry@users.noreply.github.com>
2026-08-18 16:42:01 +0200

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,2 @@
+const C="faberdrive-v1",A=["./","index.html","styles.css","app.js","manifest.webmanifest","icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.request.method==="GET"&&e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match("./")))));
+
