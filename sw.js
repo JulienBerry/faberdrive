ac8efce1a655ec48a49725d066302bc106ddf7cd
Julien <126426809+JulienBerry@users.noreply.github.com>
2026-08-18 17:24:02 +0200

    Refresh offline cache v3

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
-const C="faberdrive-v2",A=["./","index.html","styles.css","app.js","manifest.webmanifest","icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.request.method==="GET"&&e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match("./")))));
+const C="faberdrive-v3",A=["./","index.html","styles.css","app.js","manifest.webmanifest","icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener("fetch",e=>e.request.method==="GET"&&e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match("./")))));
+
 
 
